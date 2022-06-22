76b0568e621155aa4ee72c9132b068b2ac6097a1
airmanxtw <airmanx@stust.edu.tw>
2022-06-22 10:33:58 +0800

    file upload example

--- src/Components/TestRxjs.svelte
+++ src/Components/TestRxjs.svelte
@@ -8,53 +8,30 @@
         scan,
         asyncScheduler,
         subscribeOn,
-        toArray,
-        async,
+        toArray,        
         reduce,
-        map
+        map,
+        bufferCount,
+        delay,
+        of,
+        concatMap,
     } from "rxjs";
-    let total2 = 0;
 
-    // let data=[1,2,3,4,5,6,7,8,9,10];
-    // console.log('start');
-    // let t=0;
-    // for(let i=0;i<data.length;i++)
-    //     t=t+data[i];
-    // console.log(t);
-    // console.log('end');
-        
-    
-
-    // let data=[1,2,3,4,5,6,7,8,9,10];
-    // console.log('start');
-    // console.log(data.reduce((t,n)=>t+n));
-    // console.log('end');
-
-    // let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    // console.log("start");
-    // let sum = async () => {
-    //     return data.reduce((t, n) => t + n);
-    // };
-    // sum().then((t) => {
-    //     console.log(t);
-    // });
-    // console.log("end");
-
-    let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    console.log("start");
-    from(data).pipe(
-        map(n => n*n),
-        reduce((t,n)=>t+n),
-        subscribeOn(asyncScheduler),
-        toArray()
-    ).subscribe(x=>{total2 = x.at(-1)});
-    console.log('end');
-
-    let add=()=>{
-        data.push(11);
-    }
-
+    // 檔案切割範例,buffer 1024 byte,延遲1秒
+    let upload = (e) => {
+        const file = e.target.files[0];
+        range(file.size)
+            .pipe(
+                bufferCount(1024),
+                concatMap((m) => of(m).pipe(delay(1000)))
+            )
+            .subscribe((x) => {
+                // slice用法 : slice(start,end),以0為起始,end為不包含
+                const blob = file.slice(x.at(0), x.at(-1) + 1);
+                console.log(blob);
+                // ** 這裡就可以做傳送 **
+            });
+    };
 </script>
 
-<h1>{total2}</h1>
-<button on:click={add}>add</button>
+<input type="file" on:change={upload} />
