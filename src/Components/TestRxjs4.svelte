e966a489022d13e92abf2e6affe1f49ac3ad5bee
airmanxtw <airmanx@stust.edu.tw>
2022-06-26 10:57:05 +0800

    test

--- src/Components/TestRxjs4.svelte
+++ src/Components/TestRxjs4.svelte
@@ -4,15 +4,18 @@
     import { Subject, map, takeLast } from "rxjs";
     const subject = new Subject();
 
-    subject.subscribe((x) => console.log(x));
-
     let t = 0;
     let cc = () => {
         t++;
         subject.next(t);
     };
+    let dd = () => {
+        subject.subscribe((x) => console.log(x));
+    };
+
     //subject.next(1);
 </script>
 
 <div>{t}</div>
 <input type="button" on:click={cc} value="add" />
+<input type="button" on:click={dd} value="add2" />
